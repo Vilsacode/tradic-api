db894991a3ce486bacbaec22cb377b8d685fbb6d
Vilsafur <vilsafur@gmail.com>
2024-12-19 20:48:36 +0000

    dev: Ajout de composer au devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -11,6 +11,13 @@ sudo apt-get update
 # Installation de PHP 8.4
 sudo apt install -y php8.4-cli php8.4-fpm php8.4-xml
 
+# Installation de Composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
+
 # Installation Symfony CLI
 curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | sudo -E bash
 sudo apt install -y symfony-cli
